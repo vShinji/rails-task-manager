4ec037e1b1f50780f85671978beb4f24b41a70a7
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-07 15:09:22 +0900

    Tasks working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,43 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_check)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(params_check)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def params_check
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#index"
+  # get 'tasks', to: 'tasks#index'
+
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  resources :tasks
 end
